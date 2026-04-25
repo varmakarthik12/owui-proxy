5911d67557c273f3fc15fe42e8504f5b865082b1
karthik <karthiksundari2315@gmail.com>
2026-04-25 16:13:39 +0530

    feat: add version command to display build information

--- cmd/version.go
+++ cmd/version.go
@@ -11,8 +11,12 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version of owui-proxy",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("owui-proxy %s\n", Version)
-		fmt.Printf("  commit:     %s\n", Commit)
-		fmt.Printf("  built:      %s\n", BuildDate)
+		if Commit != "none" && Commit != "" {
+			fmt.Printf("  commit:     %s\n", Commit)
+		}
+		if BuildDate != "unknown" && BuildDate != "" {
+			fmt.Printf("  built:      %s\n", BuildDate)
+		}
 	},
 }
 
